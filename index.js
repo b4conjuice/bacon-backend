4a45f5cac16b62833073d30d69895e7dcca5a6ab
Daniel Lopez <dlopez807@gmail.com>
2018-03-30 12:17:04 -0700

    Add eslint formatting

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-import express from "express";
-import mongoose from "mongoose";
-import tournaments from "./tournaments";
+import express from 'express';
+import mongoose from 'mongoose';
+import tournaments from './tournaments';
 
 const app = express();
-const mongoDB = "mongodb://admin:admin@ds019856.mlab.com:19856/niello";
+const mongoDB = 'mongodb://admin:admin@ds019856.mlab.com:19856/niello';
 // const mongoDB = 'mongodb://admin:admin@ds213199.mlab.com:13199/dev';
 const db = mongoose.connection;
 
-app.set("port", process.env.PORT || 7268);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.set('port', process.env.PORT || 7268);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.route("/").get((req, res) => {
+app.route('/').get((req, res) => {
   res.json({
     success: true,
-    message: `welcome to bacon's backend`
+    message: `welcome to bacon's backend`,
   });
 });
 
-app.use("/tournaments", tournaments);
+app.use('/tournaments', tournaments);
 
-app.listen(app.get("port"));
-console.log(`bacon up on port ${app.get("port")}`);
+app.listen(app.get('port'));
+console.log(`bacon up on port ${app.get('port')}`);
 export default app;
--- tournaments.js
+++ tournaments.js
@@ -1,22 +1,18 @@
-import express from "express";
-import request from "request";
-import SmashCalTournament from "./models/smash-cal-tournament";
-import SmashGGTournament from "./models/tournament";
+import express from 'express';
+import request from 'request';
+import SmashCalTournament from './models/smash-cal-tournament';
+import SmashGGTournament from './models/tournament';
 
 const tournaments = express.Router();
 
-tournaments.route("/").get((req, res) => {
-  SmashGGTournament.find(function(err, tournaments) {
+tournaments.route('/').get((req, res) => {
+  SmashGGTournament.find((err, smashggTournaments) => {
     if (err) res.send(err);
-    res.json(
-      tournaments.sort(function(a, b) {
-        return a.date - b.date;
-      })
-    );
+    res.json(smashggTournaments.sort((a, b) => a.date - b.date));
   });
 });
 
-tournaments.route("/list").get((req, res) => {
+tournaments.route('/list').get((req, res) => {
   SmashCalTournament.find((err, smashCalTournaments) => {
     if (err) res.send(err);
     res.json(smashCalTournaments.sort((a, b) => a.startDate - b.startDate));
@@ -24,40 +20,36 @@ tournaments.route("/list").get((req, res) => {
 });
 
 tournaments
-  .route("/list/melee")
+  .route('/list/melee')
 
   .get((req, res) => {
-    SmashCalTournament.find(function(err, smashCalTournaments) {
+    SmashCalTournament.find((err, smashCalTournaments) => {
       if (err) res.send(err);
-      res.json(
-        smashCalTournaments
-          .filter(tournament => tournament.melee)
-          .sort((a, b) => a.startDate - b.startDate)
-      );
+      res.json(smashCalTournaments.filter(tournament => tournament.melee).sort((a, b) => a.startDate - b.startDate));
     });
   });
 
-tournaments.route("/slug/:slug").get((req, res) => {
+tournaments.route('/slug/:slug').get((req, res) => {
   SmashGGTournament.findOne({ slug: req.params.slug }, (err, tournament) => {
     if (err) res.send(err);
     res.json(tournament);
   });
 });
 
-tournaments.route("/smashgg/:slug").get((req, res) => {
+tournaments.route('/smashgg/:slug').get((req, res) => {
   const tournament = req.params.slug;
   const url = `https://api.smash.gg/tournament/${tournament}?expand[]=participants&mutations[]=playerData`;
 
   const options = {
-    url: url,
-    method: "GET"
+    url,
+    method: 'GET',
   };
 
   request(options, (err, response, body) => {
     if (err) res.send(err);
     const json = JSON.parse(body);
     res.json({
-      ...json
+      ...json,
     });
   });
 });
