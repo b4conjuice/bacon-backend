dd15a3dfe6751e3c42e125398de5b53bdd72d718
Daniel Lopez <dlopez807@gmail.com>
2019-01-31 11:17:59 -0800

    change name of alba route to territories

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const tournaments = require('./tournaments');
 const notes = require('./notes');
-const alba = require('./alba');
+const territories = require('./territories');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -42,6 +42,6 @@ app.route('/').get((req, res) => {
 
 app.use('/tournaments', tournaments);
 app.use('/notes', notes);
-app.use('/alba', alba);
+app.use('/territories', territories);
 
 app.listen(port, () => console.log(`bacon on port http://localhost:${port}`));
---
+++ territories.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const moment = require('moment');
+
+const Territory = require('./models/territory');
+
+const territories = express.Router();
+
+territories.use((req, res, next) => {
+  next();
+});
+
+territories
+  .route('/')
+  // create
+  .post((req, res) => {
+    const territory = new Territory();
+    territory.id = req.body.id;
+    territory.city = req.body.city;
+    territory.number = req.body.number;
+    const date = moment();
+    territory.assignments.unshift({
+      dateUgly: date,
+      date: date.format('ddd MMM DD, YYYY'),
+      ...req.body.assignments,
+    });
+    territory.save((err, savedTerritory) => {
+      if (err) res.send(err);
+      res.json({
+        message: 'territory created',
+        savedTerritory,
+      });
+    });
+  })
+  // get all
+  .get((req, res) => {
+    Territory.find((err, allTerritories) => {
+      if (err) res.send(err);
+      res.json(allTerritories);
+    });
+  });
+
+territories
+  .route('/:id/:number')
+  // get single
+  .get((req, res) => {
+    Territory.findOne(
+      {
+        id: req.params.id,
+        number: req.params.number,
+      },
+      (err, territory) => {
+        if (err) res.send(err);
+        if (territory) {
+          const { assignments } = territory || [];
+          if (req.query.q === 'latest') {
+            const latest = assignments[0];
+            res.json({ assignments: [latest] });
+          } else res.json({ assignments });
+        } else {
+          res.json({ assignments: [] });
+        }
+      }
+    );
+  })
+
+  // update
+  .put((req, res) => {
+    const date = moment();
+    const query = {
+      id: req.params.id,
+      number: req.params.number,
+    };
+    const update = {
+      $push: {
+        assignments: {
+          $each: [
+            {
+              dateUgly: date,
+              date: date.format('ddd MMM DD, YYYY'),
+              ...req.body.assignments,
+            },
+          ],
+          $position: 0,
+        },
+      },
+    };
+
+    Territory.findOneAndUpdate(
+      query,
+      update,
+      {
+        new: true,
+        upsert: true,
+        setDefaultsOnInsert: true,
+      },
+      (err, territory) => {
+        if (err) res.send(err);
+
+        const { assignments } = territory;
+        res.json({
+          assignments,
+          message: 'territory updated',
+        });
+      }
+    );
+  });
+
+// delete
+// .delete((req, res) => {
+//   Territory.remove(
+//     {
+//       _id: req.params.id,
+//     },
+//     err => {
+//       if (err) res.send(err);
+
+//       res.json({
+//         message: 'successfully deleted',
+//       });
+//     }
+//   );
+// });
+
+territories.route('/:id/:number/:assignmentid').delete((req, res) => {
+  const query = {
+    id: req.params.id,
+    number: req.params.number,
+  };
+  const update = {
+    $pull: {
+      assignments: {
+        _id: req.params.assignmentid,
+      },
+    },
+  };
+  Territory.findOneAndUpdate(
+    query,
+    update,
+    {
+      new: true,
+      upsert: true,
+      setDefaultsOnInsert: true,
+    },
+    (err, territory) => {
+      if (err) res.send(err);
+
+      const { assignments } = territory;
+      res.json({
+        assignments,
+        message: 'territory updated',
+      });
+    }
+  );
+});
+
+module.exports = territories;
